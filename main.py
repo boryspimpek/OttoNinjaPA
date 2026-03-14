7a36a445d2b74688fb57aa6ec141dbfe12951c09
boryspimpek <borysgame@gmail.com>
2026-03-14 14:02:18 +0100

    Refactor button handling in main.py to streamline action execution and improve readability

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ LF, LL, LA = cfg.LF, cfg.LL, cfg.LA
 
 moves = RobotMoves(servos, LF, RF, LL, RL, LA, RA)
 
+
+def handle_button(current_state, was_pressed, on_press, on_release=None):
+    if current_state and not was_pressed:
+        on_press()
+        return True
+    if not current_state and was_pressed:
+        if on_release is not None:
+            on_release()
+        return False
+    return was_pressed
+
+
 bt1_pressed = False
 bt2_pressed = False
 bt3_pressed = False
@@ -25,13 +37,12 @@ sw3_pressed = False
 while True:
     if cfg.tick():
 
-        # ------------------ SW 3 -----------------
-        if robot.sw3 and not sw3_pressed:
-            moves.ride_position()
-            sw3_pressed = True
-        elif not robot.sw3 and sw3_pressed:
-            servos.move_to_angles(LL, 60, RL, 120, step=10, delay=0.02)  # powrót do pozycji neutralnej
-            sw3_pressed = False
+        sw3_pressed = handle_button(
+            robot.sw3,
+            sw3_pressed,
+            moves.ride_position,
+            lambda: servos.move_to_angles(LL, 60, RL, 120, step=10, delay=0.02),  # powrót do pozycji neutralnej
+        )
 
         if sw3_pressed:
             servos.set_speed(LF, ServoController.map_joystick(robot.ly, cfg.JOY_DEAD, cfg.LF_SERVO_MIN, cfg.LF_SERVO_MAX))
@@ -40,124 +51,39 @@ while True:
         screen = robot.screen
 
         if screen == SCREEN_2:
-            # ------------------ bt1 -----------------
-            if robot.bt1 and not bt1_pressed:
-                moves.steps()
-                bt1_pressed = True
-            elif not robot.bt1 and bt1_pressed:
-                bt1_pressed = False
+            bt1_pressed = handle_button(robot.bt1, bt1_pressed, moves.steps)
 
-            # ------------------ bt2 -----------------
-            if robot.bt2 and not bt2_pressed:
-                moves.arms()
-                bt2_pressed = True
-            elif not robot.bt2 and bt2_pressed:
-                bt2_pressed = False
+            bt2_pressed = handle_button(robot.bt2, bt2_pressed, moves.arms)
 
-            # ------------------ bt3 -----------------
-            if robot.bt3 and not bt3_pressed:
-                moves.right_back()
-                bt3_pressed = True
-            elif not robot.bt3 and bt3_pressed:
-                moves.return_to_neutral()
-                bt3_pressed = False
+            bt3_pressed = handle_button(robot.bt3, bt3_pressed, moves.right_back, moves.return_to_neutral)
 
-            # ------------------ bt4 -----------------
-            if robot.bt4 and not bt4_pressed:
-                moves.right_forward()
-                bt4_pressed = True
-            elif not robot.bt4 and bt4_pressed:
-                moves.return_to_neutral()
-                bt4_pressed = False
+            bt4_pressed = handle_button(robot.bt4, bt4_pressed, moves.right_forward, moves.return_to_neutral)
             
-            # ------------------ bt5 -----------------
-            if robot.bt5 and not bt5_pressed:
-                moves.tilt()
-                bt5_pressed = True
-            elif not robot.bt5 and bt5_pressed:
-                bt5_pressed = False
+            bt5_pressed = handle_button(robot.bt5, bt5_pressed, moves.tilt)
 
-            # ------------------ bt6 -----------------
-            if robot.bt6 and not bt6_pressed:
-                moves.wave()
-                bt6_pressed = True
-            elif not robot.bt6 and bt6_pressed:
-                bt6_pressed = False
+            bt6_pressed = handle_button(robot.bt6, bt6_pressed, moves.wave)
 
-            # ------------------ bt7 -----------------
-            if robot.bt7 and not bt7_pressed:
-                moves.left_back()
-                bt7_pressed = True  
-            elif not robot.bt7 and bt7_pressed:
-                moves.return_to_neutral()
-                bt7_pressed = False
+            bt7_pressed = handle_button(robot.bt7, bt7_pressed, moves.left_back, moves.return_to_neutral)
 
-            # ------------------ bt8 -----------------
-            if robot.bt8 and not bt8_pressed:
-                moves.left_forward()
-                bt8_pressed = True
-            elif not robot.bt8 and bt8_pressed:
-                moves.return_to_neutral()
-                bt8_pressed = False
+            bt8_pressed = handle_button(robot.bt8, bt8_pressed, moves.left_forward, moves.return_to_neutral)
 
         elif screen == SCREEN_3:
             # Drugi zestaw akcji – wykorzystanie dotąd nieużywanych ruchów
 
-            # ------------------ bt1 -----------------
-            if robot.bt1 and not bt1_pressed:
-                moves.weird()
-                bt1_pressed = True
-            elif not robot.bt1 and bt1_pressed:
-                bt1_pressed = False
+            bt1_pressed = handle_button(robot.bt1, bt1_pressed, moves.weird)
 
-            # ------------------ bt2 -----------------
-            if robot.bt2 and not bt2_pressed:
-                moves.balerina()
-                bt2_pressed = True
-            elif not robot.bt2 and bt2_pressed:
-                moves.return_to_neutral()
-                bt2_pressed = False
+            bt2_pressed = handle_button(robot.bt2, bt2_pressed, moves.balerina, moves.return_to_neutral)
 
-            # ------------------ bt3 -----------------
-            if robot.bt3 and not bt3_pressed:
-                moves.boogie()
-                bt3_pressed = True
-            elif not robot.bt3 and bt3_pressed:
-                bt3_pressed = False
+            bt3_pressed = handle_button(robot.bt3, bt3_pressed, moves.boogie)
 
-            # ------------------ bt4 -----------------
-            if robot.bt4 and not bt4_pressed:
-                moves.spin()
-                bt4_pressed = True
-            elif not robot.bt4 and bt4_pressed:
-                bt4_pressed = False
+            bt4_pressed = handle_button(robot.bt4, bt4_pressed, moves.spin)
             
-            # ------------------ bt5 -----------------
-            if robot.bt5 and not bt5_pressed:
-                moves.toes()
-                bt5_pressed = True
-            elif not robot.bt5 and bt5_pressed:
-                bt5_pressed = False
+            bt5_pressed = handle_button(robot.bt5, bt5_pressed, moves.toes)
 
-            # ------------------ bt6 -----------------
-            if robot.bt6 and not bt6_pressed:
-                moves.heels()
-                bt6_pressed = True
-            elif not robot.bt6 and bt6_pressed:
-                bt6_pressed = False
+            bt6_pressed = handle_button(robot.bt6, bt6_pressed, moves.step_left)
 
-            # ------------------ bt7 -----------------
-            if robot.bt7 and not bt7_pressed:
-                moves.heels_ride()
-                bt7_pressed = True  
-            elif not robot.bt7 and bt7_pressed:
-                bt7_pressed = False
+            bt7_pressed = handle_button(robot.bt7, bt7_pressed, moves.step_right)
 
-            # ------------------ bt8 -----------------
-            if robot.bt8 and not bt8_pressed:
-                moves.circles()
-                bt8_pressed = True
-            elif not robot.bt8 and bt8_pressed:
-                bt8_pressed = False
+            bt8_pressed = handle_button(robot.bt8, bt8_pressed, moves.circles)
 
     time.sleep_ms(10)
